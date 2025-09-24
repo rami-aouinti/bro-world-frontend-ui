1082edc8a27c8049838be7f80bda4db3a8dfe9b5
Rami <rami.aouinti@gmail.com>
2025-09-24 22:33:17 +0200

    feat: integrate vuetify

---
+++ plugins/vuetify.ts
@@ -0,0 +1,15 @@
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+    ssr: true,
+  });
+
+  nuxtApp.vueApp.use(vuetify);
+});
