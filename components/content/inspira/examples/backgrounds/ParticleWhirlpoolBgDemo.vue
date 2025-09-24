f7cec83a48bd75a79f6758e188b08864b88d4966
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-09-24 16:00:03 +0200

    Initial commit

---
+++ components/content/inspira/examples/backgrounds/ParticleWhirlpoolBgDemo.vue
@@ -0,0 +1,12 @@
+<template>
+  <ParticleWhirlpoolBg
+    :blur="2"
+    class="h-96"
+  >
+    <div
+      class="flex size-full flex-col items-center justify-center font-heading text-5xl text-white"
+    >
+      Cool Background
+    </div>
+  </ParticleWhirlpoolBg>
+</template>
---
+++ components/content/inspira/examples/morphing-tabs/MorphingTabsDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <MorphingTabs
+    :tabs="tabs"
+    :active-tab="activeTab"
+    @update:active-tab="activeTab = $event"
+  />
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const tabs = ["Home", "Apps", "About", "My"];
+const activeTab = ref(tabs[0]);
+</script>
