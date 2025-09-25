5a3dee27bfabf547121fcea2aec2f689fd406b52
Rami <rami.aouinti@gmail.com>
2025-09-25 17:25:07 +0200

    Add Italian, Spanish, and Russian translations

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -2,17 +2,23 @@ import ar from "./locales/ar.json";
 import de from "./locales/de.json";
 import en from "./locales/en.json";
 import fr from "./locales/fr.json";
+import es from "./locales/es.json";
+import it from "./locales/it.json";
+import ru from "./locales/ru.json";
 
 export default defineI18nConfig(() => ({
   legacy: false,
   globalInjection: true,
   locale: "en",
   fallbackLocale: "en",
-  availableLocales: ["en", "fr", "de", "ar"],
+  availableLocales: ["en", "fr", "de", "es", "it", "ru", "ar"],
   messages: {
     en,
     fr,
     de,
+    es,
+    it,
+    ru,
     ar,
   },
 }));
