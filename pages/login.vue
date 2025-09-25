2a1d0ac0e6e9ae6d4b4cab75d66b43c5f5a74da6
Rami <rami.aouinti@gmail.com>
2025-09-25 16:46:59 +0200

    refactor: extract auth forms into components

---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="py-12" style="margin-top: -70px;">
+    <v-card-text class="text-medium-emphasis pa-1">
+      <AuthLoginForm />
+    </v-card-text>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import AuthLoginForm from "~/components/auth/LoginForm.vue";
+</script>
+
---
+++ pages/register.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="py-12" style="margin-top: -70px;">
+    <v-card-text class="text-medium-emphasis pa-0">
+      <AuthRegisterForm />
+    </v-card-text>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import AuthRegisterForm from "~/components/auth/RegisterForm.vue";
+</script>
+
