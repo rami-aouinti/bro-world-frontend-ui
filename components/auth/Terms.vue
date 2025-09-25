d3934e4be0255829820e226c63c79f7515d5ff7d
Rami <rami.aouinti@gmail.com>
2025-09-25 16:32:00 +0200

    feat: add localized auth pages

---
+++ components/auth/Terms.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="space-y-4 text-body-2">
+    <p>
+      {{ t('register.termsIntro') }}
+    </p>
+    <ul class="ms-4 list-disc space-y-2">
+      <li>{{ t('register.termsPoint1') }}</li>
+      <li>{{ t('register.termsPoint2') }}</li>
+      <li>{{ t('register.termsPoint3') }}</li>
+    </ul>
+    <p>
+      {{ t('register.termsOutro') }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+</script>
