06d94ba027f7e0bde2585c9062e689960f15cbe7
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-09-29 16:32:18 +0200

    Initial commit

--- lib/material-dashboard-vendors.ts
+++ lib/material-dashboard-vendors.ts
@@ -1,12 +1,6 @@
 import { defineAsyncComponent, type AsyncComponentLoader } from 'vue'
 
 const vendorStyleLoaders = {
-  sweetalert2: () => import('~/assets/styles/material-dashboard/vendors/lazy/sweetalert2.scss'),
-  fullcalendar: () => import('~/assets/styles/material-dashboard/vendors/lazy/fullcalendar.scss'),
-  quill: () => import('~/assets/styles/material-dashboard/vendors/lazy/quill.scss'),
-  dropzone: () => import('~/assets/styles/material-dashboard/vendors/lazy/dropzone.scss'),
-  photoswipe: () => import('~/assets/styles/material-dashboard/vendors/lazy/photoswipe.scss'),
-  kanban: () => import('~/assets/styles/material-dashboard/vendors/lazy/kanban.scss'),
 } as const
 
 export type MaterialDashboardVendor = keyof typeof vendorStyleLoaders
