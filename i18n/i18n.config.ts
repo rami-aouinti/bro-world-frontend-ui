836be78bd198ff62c3f42b1a6121d9400f31e5e4
Rami <rami.aouinti@gmail.com>
2025-10-01 04:31:02 +0200

    Add Simplified Chinese locale to language menu

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -5,13 +5,14 @@ import fr from "./locales/fr.json";
 import es from "./locales/es.json";
 import it from "./locales/it.json";
 import ru from "./locales/ru.json";
+import zhCN from "./locales/zh-cn.json";
 
 export default defineI18nConfig(() => ({
   legacy: false,
   globalInjection: true,
   locale: "en",
   fallbackLocale: "en",
-  availableLocales: ["en", "fr", "de", "es", "it", "ru", "ar"],
+  availableLocales: ["en", "fr", "de", "es", "it", "ru", "ar", "zh-cn"],
   messages: {
     en,
     fr,
@@ -20,5 +21,6 @@ export default defineI18nConfig(() => ({
     it,
     ru,
     ar,
+    "zh-cn": zhCN,
   },
 }));
