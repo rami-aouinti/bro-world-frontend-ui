701c4c699b40997560fce8d9bb5ad1c5822df598
Rami <rami.aouinti@gmail.com>
2025-10-17 01:32:41 +0200

    Guard app icon tooltips until client hydration

--- components/layout/app-bar/AppIconBar.vue
+++ components/layout/app-bar/AppIconBar.vue
@@ -1,25 +1,43 @@
 <template>
   <div :class="iconBarClasses">
-    <v-tooltip
-      v-for="icon in props.appIcons"
-      :key="icon.label"
-      :text="t(icon.label)"
-      :aria-label="t(icon.label)"
-    >
-      <template #activator="{ props: tooltipProps }">
-        <v-btn
-          v-bind="tooltipProps"
-          :aria-label="t(icon.label)"
-          :class="props.iconTriggerClasses"
-          :theme="props.isDark ? 'dark' : 'light'"
-        >
-          <AppIcon
-            :name="icon.name"
-            :size="26"
-          />
-        </v-btn>
-      </template>
-    </v-tooltip>
+    <template v-if="isHydrated">
+      <v-tooltip
+        v-for="icon in props.appIcons"
+        :key="icon.label"
+        :text="t(icon.label)"
+        :aria-label="t(icon.label)"
+      >
+        <template #activator="{ props: tooltipProps }">
+          <v-btn
+            v-bind="tooltipProps"
+            :aria-label="t(icon.label)"
+            :class="props.iconTriggerClasses"
+            :theme="props.isDark ? 'dark' : 'light'"
+          >
+            <AppIcon
+              :name="icon.name"
+              :size="26"
+            />
+          </v-btn>
+        </template>
+      </v-tooltip>
+    </template>
+    <template v-else>
+      <v-btn
+        v-for="icon in props.appIcons"
+        :key="icon.label"
+        :aria-label="t(icon.label)"
+        :title="t(icon.label)"
+        :class="props.iconTriggerClasses"
+        :theme="props.isDark ? 'dark' : 'light'"
+        :ripple="false"
+      >
+        <AppIcon
+          :name="icon.name"
+          :size="26"
+        />
+      </v-btn>
+    </template>
   </div>
 </template>
 
