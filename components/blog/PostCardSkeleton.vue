7f06cd4deb2ea80598070f34a6d4cb1785e4cbeb
Rami <rami.aouinti@gmail.com>
2025-10-18 21:19:38 +0200

    Add comment thread skeleton to PostCard loader

--- components/blog/PostCardSkeleton.vue
+++ components/blog/PostCardSkeleton.vue
@@ -11,6 +11,23 @@
     <PostMetaSkeleton class="mb-4" />
     <BlogPostContentSkeleton class="mb-6" />
     <BlogPostReactCardSkeleton />
+    <div
+      v-if="showComments"
+      class="post-skeleton__comments"
+    >
+      <div class="post-skeleton__comments-header">
+        <v-skeleton-loader
+          class="post-skeleton__sort"
+          type="chip"
+          width="140"
+          height="36"
+        />
+      </div>
+      <CommentThreadSkeleton
+        class="post-skeleton__comment-thread"
+        :show-composer="showComposer"
+      />
+    </div>
   </v-card>
 </template>
 
@@ -19,10 +36,26 @@ import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 import BlogPostContentSkeleton from "./BlogPostContentSkeleton.vue";
 import BlogPostReactCardSkeleton from "./BlogPostReactCardSkeleton.vue";
+import CommentThreadSkeleton from "./CommentThreadSkeleton.vue";
 import PostMetaSkeleton from "./PostMetaSkeleton.vue";
 
 const { t } = useI18n();
+const props = withDefaults(
+  defineProps<{
+    showCommentSkeleton?: boolean;
+    showComposer?: boolean;
+  }>(),
+  {
+    showCommentSkeleton: true,
+    showComposer: false,
+  },
+);
+
 const loadingLabel = computed(() => t("blog.posts.loading"));
+const showComments = computed(() => props.showCommentSkeleton);
+const showComposer = computed(
+  () => showComments.value && props.showComposer,
+);
 </script>
 
 <style scoped>
@@ -33,4 +66,19 @@ const loadingLabel = computed(() => t("blog.posts.loading"));
   padding: 1.5rem;
 }
 
+.post-skeleton__comments {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 0.5rem;
+}
+
+.post-skeleton__comments-header {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.post-skeleton__sort {
+  border-radius: 9999px;
+}
 </style>
