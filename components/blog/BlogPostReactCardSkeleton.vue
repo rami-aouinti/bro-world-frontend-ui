d4f565022231fa4ecbaf6e3f2b2f7410bfd39d16
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-10-18 22:08:40 +0200

    changes

--- components/blog/BlogPostReactCardSkeleton.vue
+++ components/blog/BlogPostReactCardSkeleton.vue
@@ -5,49 +5,6 @@
     aria-live="polite"
     :aria-label="loadingLabel"
   >
-    <div class="flex flex-wrap items-center justify-between gap-3">
-      <div class="flex min-w-[96px] items-center gap-3">
-        <div class="relative h-[26px] w-[60px]">
-          <v-skeleton-loader
-            v-for="index in 3"
-            :key="`bubble-${index}`"
-            type="avatar"
-            class="absolute top-0 h-[26px] w-[26px] rounded-full ring-1 ring-black/5"
-            :style="{ left: `${(index - 1) * 16}px`, zIndex: 4 - index }"
-          />
-        </div>
-        <v-skeleton-loader
-          type="text"
-          width="48"
-          height="16"
-          class="rounded"
-        />
-      </div>
-      <div class="flex min-w-[80px] items-center justify-end gap-2">
-        <v-skeleton-loader
-          v-for="index in 2"
-          :key="`metric-${index}`"
-          type="chip"
-          width="64"
-          height="20"
-        />
-      </div>
-    </div>
-    <div class="flex items-center gap-2">
-      <v-skeleton-loader
-        type="avatar"
-        width="44"
-        height="44"
-        class="rounded-full"
-      />
-      <v-skeleton-loader
-        v-for="index in 2"
-        :key="`action-${index}`"
-        type="button"
-        width="128"
-        height="40"
-      />
-    </div>
   </div>
 </template>
 
