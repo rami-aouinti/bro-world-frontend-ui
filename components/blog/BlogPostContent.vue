0f0bc8a7bede33e8b4b79685d1d73aadbb4b55ec
Rami <rami.aouinti@gmail.com>
2025-10-19 23:36:43 +0200

    Adjust blog post card heading hierarchy

--- components/blog/BlogPostContent.vue
+++ components/blog/BlogPostContent.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="space-y-2 py-4 my-3 px-2 w-full">
-    <h4 class="text font-semibold leading-tight text-foreground">
+    <h2 class="text font-semibold leading-tight text-foreground">
       {{ props.title }}
-    </h4>
+    </h2>
     <p
       v-if="props.summary"
       class="text-base leading-relaxed text-slate-500 mx-2"
