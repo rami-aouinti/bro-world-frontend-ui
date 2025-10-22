2038495d003177da050c24fe340ceff3aefaf3b8
Rami <rami.aouinti@gmail.com>
2025-10-22 03:06:41 +0200

    Revert "Fix sticky sidebar offsets for layout drawers"

--- components/layout/AppSidebarRight.vue
+++ components/layout/AppSidebarRight.vue
@@ -53,7 +53,5 @@ const sticky = computed(() => props.sticky);
 
 .app-sidebar--sticky {
   position: sticky;
-  top: calc(var(--layout-inset-top, var(--app-bar-height, 50px)) + clamp(16px, 3vw, 32px));
-  align-self: flex-start;
 }
 </style>
