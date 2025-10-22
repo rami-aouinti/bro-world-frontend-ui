78a8264b74feaba1053557e2ecf811833ec5ab00
Rami <rami.aouinti@gmail.com>
2025-10-22 02:58:33 +0200

    Adjust sidebar sticky offsets

--- components/layout/AppSidebar.vue
+++ components/layout/AppSidebar.vue
@@ -509,6 +509,8 @@ function resolveLocalizedPath(target?: string): string | undefined {
 
 .app-sidebar--sticky {
   position: sticky;
+  top: calc(var(--layout-inset-top, var(--app-bar-height, 50px)) + clamp(16px, 3vw, 32px));
+  align-self: flex-start;
 }
 
 .sidebar-group {
